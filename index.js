98cee5dde0eec0f92a3948fd16f009e07454aa3f
B Terbush <brittanyterbush@gmail.com>
2018-03-17 23:54:05 -0700

    Configure server to render index.html file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '/public') });
+});
 
 // app listening on port 5000 if not otherwise designated (for Heroku deployment)
 const PORT = process.env.PORT || 5000;
