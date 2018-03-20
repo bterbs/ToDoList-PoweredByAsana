14f6a74138098eabbf646000cdb0213c6acc3c66
B Terbush <brittanyterbush@gmail.com>
2018-03-19 23:17:24 -0700

    Add scripts file without personal access token

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  $();
+  let url = 'https://app.asana.com/api/1.0/projects/600042698902717/tasks';
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    headers: { Authorization: 'Bearer asanaPersonalAccesToken' }
+  })
+    .done(function(data) {
+      console.log('success', data);
+    })
+    .fail(function(xhr) {
+      console.log('Error, Will Robinson!', xhr);
+    });
 });
