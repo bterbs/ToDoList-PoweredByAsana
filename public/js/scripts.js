71674d7a6d9de0538dafac50482c329aa6419bd3
B Terbush <brittanyterbush@gmail.com>
2018-03-20 14:11:03 -0700

    Style checkboxes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,34 @@
 $(document).ready(function() {
-  $.ajax({
-    url: '/asana/search',
-    type: 'GET'
-  })
-    .done(function(results) {
-      console.log('success!', results);
-      const taskNodes = [];
-      results.data.data.forEach(task => {
-        taskNodes.push(
-          $(`
-          <div class="list-item">
-          <p>&#x2714</p>
-          <li class="task-item">${task.name}</li>
-          <button id=${task._id}>Details</button>
-          </div>`)
-        );
-      });
-      console.log(taskNodes);
-      $('#list-output').empty();
-      taskNodes.forEach(node => {
-        $('#list-output').append(node);
-      });
+  const getTasks = () => {
+    const param = 'search';
+
+    $.ajax({
+      url: `/asana/${param}`,
+      type: 'GET'
     })
-    .fail(function(xhr) {
-      console.log('Error, Will Robinson!', xhr);
-    });
+      .done(function(results) {
+        console.log('success!', results);
+        const taskNodes = [];
+        results.data.data.forEach(task => {
+          taskNodes.push(
+            $(`
+              <div class="list-item">
+              <button id=${task._id} class="complete-task-btn">&#x269E</button>
+              <li class="task-item">${task.name}</li>
+              </div>`)
+          );
+        });
+        console.log(taskNodes);
+        $('#list-output').empty();
+        $('#list-output').append('<h4>Project</h4>');
+        taskNodes.forEach(node => {
+          $('#list-output').append(node);
+        });
+      })
+      .fail(function(xhr) {
+        console.log('Error, Will Robinson!', xhr);
+      });
+  };
+
+  $('#get-btn').on('click', getTasks);
 });
