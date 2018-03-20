8a1c7de5cea9afb1f3ebab0b3248d0d438144811
B Terbush <brittanyterbush@gmail.com>
2018-03-20 12:05:04 -0700

    Style ajax call results into formatted list

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,6 +5,22 @@ $(document).ready(function() {
   })
     .done(function(results) {
       console.log('success!', results);
+      const taskNodes = [];
+      results.data.data.forEach(task => {
+        taskNodes.push(
+          $(`
+          <div class="list-item">
+          <p>&#x2714</p>
+          <li class="task-item">${task.name}</li>
+          <button id=${task._id}>Details</button>
+          </div>`)
+        );
+      });
+      console.log(taskNodes);
+      $('#list-output').empty();
+      taskNodes.forEach(node => {
+        $('#list-output').append(node);
+      });
     })
     .fail(function(xhr) {
       console.log('Error, Will Robinson!', xhr);
