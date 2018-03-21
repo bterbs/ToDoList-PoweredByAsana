c253edc47d79882cfc1c0f8bcc8781f94c4c3f34
B Terbush <brittanyterbush@gmail.com>
2018-03-21 12:33:40 -0700

    WIP: massive overhaul to satisfy front end routing requirement; now using ejs template rendering

---
+++ server/routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const AsanaApi = require('./asana_api.js');
+
+// index routes
+router.get('/', (req, res, next) => {
+  return res.render('events/index.ejs');
+});
+
+// route to access Asana's api endpoint
+router.get('/asana', (req, res) => {
+  const pID = req.query.project;
+  AsanaApi.search(pID)
+    .then(results => {
+      res.set('Access-Control-Allow-Origin', '*');
+      console.log(results.data.data);
+      if (req.xhr) {
+        // request was AJAX (XHR)
+        res.json({ data: results.data.data });
+      } else {
+        // render html template instead
+        res.render('events/results.ejs', { data: results.data.data });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      return { error: true, message: err.message };
+    });
+});
+
+module.exports = router;
