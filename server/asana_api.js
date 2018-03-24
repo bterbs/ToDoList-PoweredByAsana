7dc30edf51c5cda0920758ceec6aae36c74a23e4
B Terbush <brittanyterbush@gmail.com>
2018-03-24 14:21:15 -0700

    refactor search function

--- server/asana_api.js
+++ server/asana_api.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 const { API_KEY } = require('../config.js').keys;
 
-function search(pID) {
-  let projectID = pID;
+function search(projectID) {
+  // let projectID = pID;
   return axios
     .get(`https://app.asana.com/api/1.0/projects/${projectID}/tasks`, {
       headers: { Authorization: `Bearer ${API_KEY}` }
